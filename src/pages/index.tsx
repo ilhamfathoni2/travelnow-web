1f76b064e5ea80be14ac62396c8a668a879fa717
ilham.fathoni <ilham.fathoni>
2023-06-08 11:47:43 +0700

    refactor home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,17 +3,16 @@ import Head from "next/head";
 import { ArrowRightIcon, MapPinIcon } from "@heroicons/react/24/outline";
 import {
   aboutBeach,
-  // homeBg,
-  // homeTrees,
-  // homeLake,
-  // homeMountain,
-  // homeBeach,
+  homeBg,
+  homeTrees,
+  homeLake,
+  homeMountain,
+  homeBeach,
   popularMountain,
   popularForest,
-  popularLake
+  popularLake,
 } from "../../public/assets/img";
 import Navbar from "@/component/navbar/nav";
-import HomeSection from "@/component/home";
 
 export default function Home() {
   return (
@@ -28,7 +27,91 @@ export default function Home() {
       {/* ----- MAIN CONTENT ----- */}
       <main className="main">
         {/* ---- HOME ---- */}
-        <HomeSection/>
+        <section className="home section" id="home">
+          <Image
+            src={homeBg}
+            alt="background image"
+            quality={100}
+            className="home__bg"
+          />
+          <div className="home__shadow"></div>
+
+          <div className="home__container container grid">
+            <div className="text-center">
+              <h3 className="text-[16px] mb-[1rem] font-montserrat">
+                Welcome To Travelnow
+              </h3>
+
+              <h1 className="text-[40px] mb-[1rem] font-popins">
+                Explore <br /> The World
+              </h1>
+
+              <p className="mb-[2rem] text-gray-300 font-normal font-montserrat">
+                Live the trips exploring the world. Discover paradises, islands,
+                mountains, much more, get your trip now
+              </p>
+
+              <a href="#" className="button">
+                <p>Start Your Journey </p>{" "}
+                <ArrowRightIcon width={16} className="arrow-right-icon" />
+              </a>
+            </div>
+
+            <div className="home__cards grid">
+              <article className="home__card">
+                <Image
+                  src={homeTrees}
+                  alt="home img"
+                  quality={100}
+                  className="home__card-img w-full object-cover"
+                />
+                <h3 className="absolute left-[1rem] bottom-[1rem] z-10">
+                  Croatia
+                </h3>
+                <div className="home__card-shadow"></div>
+              </article>
+
+              <article className="home__card">
+                <Image
+                  src={homeLake}
+                  alt="home img"
+                  quality={100}
+                  className="home__card-img w-full object-cover"
+                />
+                <h3 className="absolute left-[1rem] bottom-[1rem] z-10">
+                  Iceland
+                </h3>
+                <div className="home__card-shadow"></div>
+              </article>
+
+              <article className="home__card">
+                <Image
+                  src={homeMountain}
+                  alt="home img"
+                  quality={100}
+                  className="home__card-img w-full object-cover"
+                />
+                <h3 className="absolute left-[1rem] bottom-[1rem] z-10">
+                  Italy
+                </h3>
+                <div className="home__card-shadow"></div>
+              </article>
+
+              <article className="home__card">
+                <Image
+                  src={homeBeach}
+                  alt="home img"
+                  quality={100}
+                  className="home__card-img w-full object-cover"
+                />
+                <h3 className="home__card-title absolute left-[1rem] bottom-[1rem] z-10">
+                  Spain
+                </h3>
+                <div className="home__card-shadow"></div>
+              </article>
+            </div>
+          </div>
+        </section>
 
         {/* ---- ABOUT ---- */}
         <section className="about section" id="about">
@@ -81,9 +164,7 @@ export default function Home() {
                 <div className="popular__shadow"></div>
               </div>
 
-              <h2 className="popular__title">
-                Logan Mountain
-              </h2>
+              <h2 className="popular__title">Logan Mountain</h2>
 
               <div className="popular__location">
                 <MapPinIcon width={16} />
@@ -102,9 +183,7 @@ export default function Home() {
                 <div className="popular__shadow"></div>
               </div>
 
-              <h2 className="popular__title">
-                Spike Forest
-              </h2>
+              <h2 className="popular__title">Spike Forest</h2>
 
               <div className="popular__location">
                 <MapPinIcon width={16} />
@@ -123,9 +202,7 @@ export default function Home() {
                 <div className="popular__shadow"></div>
               </div>
 
-              <h2 className="popular__title">
-                Garda Lake
-              </h2>
+              <h2 className="popular__title">Garda Lake</h2>
 
               <div className="popular__location">
                 <MapPinIcon width={16} />
